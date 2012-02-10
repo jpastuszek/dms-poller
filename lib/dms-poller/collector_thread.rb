a51d439eb8c09a293aa7c61b889882779b83e69f
Jakub Pastuszek <jpastuszek@gmail.com>
2012-02-10 16:43:27 +0100

    thread error and exit handling

--- lib/dms-poller/collector_thread.rb
+++ lib/dms-poller/collector_thread.rb
@@ -1,29 +1,39 @@
 class CollectorThread < Thread
-	def initialize(collector_bind_address, data_processor_address, location)
+	def initialize(shutdown_queue, collector_bind_address, data_processor_address, location)
 		log.info "Binding collector socket at: #{collector_bind_address}"
 		log.info "Connecting collector with data porcessor at: #{data_processor_address}"
 
 		super do
-			abort_on_exception = true
+			begin
+				ZeroMQ.new do |zmq|
+					begin
+						zmq.pull_bind(collector_bind_address) do |pull|
+							zmq.push_connect(data_processor_address, 1, 2.5) do |push|
+								loop do
+									message = pull.recv
+									if message.class != RawDatum
+										log.warn "collected message of type: #{message.class.name}, expected RawDatum"
+										next
+									end
 
-			ZeroMQ.new do |zmq|
-				zmq.pull_bind(collector_bind_address) do |pull|
-					zmq.push_connect(data_processor_address) do |push|
-						loop do
-							message = pull.recv
-							if message.class != RawDatum
-								log.warn "collected message of type: #{message.class.name}, expected RawDatum"
-								next
+									raw_data_point = message.to_raw_data_point(location)
+
+									log.debug "sending #{raw_data_point}"
+									push.send raw_data_point
+								end
 							end
-
-							raw_data_point = message.to_raw_data_point(location, Time.now.utc.to_i)
-
-							log.debug "sending #{raw_data_point}"
-							push.send raw_data_point
-						end
+						end 
+					ensure
+						log.info "waiting for messages to be sent..."
 					end
 				end 
-			end 
+			rescue Interrupt
+				log.info "interrupted"
+			rescue => e
+				log.fatal "got error: #{e}: #{e.message}"
+			ensure
+				shutdown_queue.push 'scheduler'
+			end
 		end
 	end
 end
--- lib/dms-poller/scheduler_thread.rb
+++ lib/dms-poller/scheduler_thread.rb
@@ -2,22 +2,31 @@ require 'dms-poller/scheduler_run_process_pool'
 require 'dms-poller/probe_scheduler'
 
 class SchedulerThread < Thread
-	def initialize(poller_modules, collector_bind_address, quantum, time_scale, runs, startup_run, process_limit, process_time_out)
+	def initialize(shutdown_queue, poller_modules, collector_bind_address, quantum, time_scale, runs, startup_run, process_limit, process_time_out)
 		probe_scheduler = ProbeScheduler.new(quantum, time_scale)
 		probe_scheduler.schedule_modules(poller_modules)
 
 		# start thread
 		super do
-			abort_on_exception = true
-			SchedulerRunProcessPool.new(process_limit, process_time_out) do |process_pool|
-				probe_scheduler.run!(runs, startup_run) do |probes, run_no|
-					process_pool.fork_process(probes, collector_bind_address, run_no)
+			begin
+				SchedulerRunProcessPool.new(process_limit, process_time_out) do |process_pool|
+					begin
+						probe_scheduler.run!(runs, startup_run) do |probes, run_no|
+							process_pool.fork_process(probes, collector_bind_address, run_no)
+						end
+
+						log.info "scheduler finished #{runs} runs, shutting down..."
+					ensure
+						pids = process_pool.running_pids
+						log.info "watining for #{pids.length} scheduler run processes to finish: pids: #{pids.join(', ')}" unless pids.empty?
+					end
 				end
-
-				log.info "scheduler finished #{runs} runs, shutting down..."
-
-				pids = process_pool.running_pids
-				log.info "watining for #{pids.length} scheduler run processes to finish: pids: #{pids.join(', ')}" unless pids.empty?
+			rescue Interrupt
+				log.info "interrupted"
+			rescue => e
+				log.fatal "got error: #{e}: #{e.message}"
+			ensure
+				shutdown_queue.push 'scheduler'
 			end
 		end
 	end
