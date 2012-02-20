49edc7a8f5b0a6389090546000676474c0075c6e
Jakub Pastuszek <jpastuszek@gmail.com>
2012-02-20 20:54:02 +0100

    copyright

--- features/step_definitions/dms-poller_steps.rb
+++ features/step_definitions/dms-poller_steps.rb
@@ -1,3 +1,20 @@
+# Copyright (c) 2012 Jakub Pastuszek
+#
+# This file is part of Distributed Monitoring System.
+#
+# Distributed Monitoring System is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Distributed Monitoring System is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Distributed Monitoring System.  If not, see <http://www.gnu.org/licenses/>.
+
 Given /data processor stub running at (.+) that expects (.+) messages/ do |data_processor_address, message_count|
 	
 	@data_processor_stub_pid, @data_processor_stdout_thread = spawn('dms-data-processor-stub', "--bind-address #{data_processor_address} --message-count #{message_count}")
