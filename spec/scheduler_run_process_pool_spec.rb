1aa9f2ed82f49084b6e60dd72cdefad221a80c5a
Jakub Pastuszek <jpastuszek@gmail.com>
2012-04-02 22:55:23 +0200

    capturing log output

--- spec/scheduler_run_process_pool_spec.rb
+++ spec/scheduler_run_process_pool_spec.rb
@@ -19,51 +19,55 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe SchedulerRunProcessPool do
 	before :all do
-		m_system = PollerModule.new('system') do
-			probe('sysstat') do
-				collect 'CPU usage/total', 'idle', 3123
-				collect 'system/process', 'blocked', 0
-			end.schedule_every 10.second
+		Capture.stderr do
+			m_system = PollerModule.new('system') do
+				probe('sysstat') do
+					collect 'CPU usage/total', 'idle', 3123
+					collect 'system/process', 'blocked', 0
+				end.schedule_every 10.second
 
-			probe('memory') do
-				collect 'system/memory', 'total', 8182644
-				collect 'system/memory', 'free', 5577396
-				collect 'system/memory', 'buffers', 254404
-			end.schedule_every 30.seconds
+				probe('memory') do
+					collect 'system/memory', 'total', 8182644
+					collect 'system/memory', 'free', 5577396
+					collect 'system/memory', 'buffers', 254404
+				end.schedule_every 30.seconds
+			end
+
+			m_jmx = PollerModule.new('jmx') do
+				probe('gc') do
+					collect 'JMX/1234/GC/PermGen', 'collections', 231
+				end.schedule_every 60.seconds
+			end
+
+			@probes = m_system.probes + m_jmx.probes
+			@addr = 'ipc:///tmp/dms-poller-test'
 		end
-
-		m_jmx = PollerModule.new('jmx') do
-			probe('gc') do
-				collect 'JMX/1234/GC/PermGen', 'collections', 231
-			end.schedule_every 60.seconds
-		end
-
-		@probes = m_system.probes + m_jmx.probes
-		@addr = 'ipc:///tmp/dms-poller-test'
 	end
 
 	it "should produce RawDataPoint objects on ZeroMQ endpoint" do
 		ZeroMQ.new do |zmq|
 			zmq.pull_bind(@addr) do |pull|
-				SchedulerRunProcessPool.new(10, 2) do |pool|
-					pool.fork_process(@probes, 'magi', @addr, 0)
-				end
+				Capture.stderr do
+					SchedulerRunProcessPool.new(10, 2) do |pool|
+						pool.fork_process(@probes, 'magi', @addr, 0)
+					end
 
-				raw_data = []
-				pull.on RawDataPoint do |message|
-					raw_data << message
-				end
+					raw_data = []
+					pull.on RawDataPoint do |message|
+						raw_data << message
+					end
 
-				5.times do
-					pull.receive!
-				end
+					5.times do
+						pull.receive!
+					end
 
-				raw_data.should have(5).raw_data_point
-				raw_data.first.should be_a RawDataPoint
-				raw_data.first.location.should == 'magi'
-				raw_data.first.path.should == 'CPU usage/total'
-				raw_data.first.component.should == 'idle'
-				raw_data.first.value.should == 3123
+					raw_data.should have(5).raw_data_point
+					raw_data.first.should be_a RawDataPoint
+					raw_data.first.location.should == 'magi'
+					raw_data.first.path.should == 'CPU usage/total'
+					raw_data.first.component.should == 'idle'
+					raw_data.first.value.should == 3123
+				end
 			end
 		end
 	end
